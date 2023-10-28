fdc92dcc9a39a9e8243460c19e74918ff5b9742e
youssrif2018 <youssrif2018@gmail.com>
2023-10-28 10:49:13 +0100

    update css

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function convertTemperature() {
-  var celsius = parseFloat(document.getElementById("celsius").value);
-  var fahrenheit = (celsius * 9/5) + 32;
-  document.getElementById("fahrenheit").value = fahrenheit;
+  var celsiusInput = document.getElementById('celsius');
+  var fahrenheitInput = document.getElementById('fahrenheit');
+  var celsius = parseFloat(celsiusInput.value);
+  var fahrenheit = (celsius * 9) / 5 + 32;
+  fahrenheitInput.value = fahrenheit;
+
+  celsiusInput.classList.add('temperature-input-converted');
+  fahrenheitInput.classList.add('temperature-input-converted');
+  document
+    .querySelector('.temperature-heading')
+    .classList.add('temperature-heading-converted');
+  document
+    .querySelector('.convert-button')
+    .classList.add('convert-button-converted');
+
+  setTimeout(function () {
+    celsiusInput.classList.remove('temperature-input-converted');
+    fahrenheitInput.classList.remove('temperature-input-converted');
+    document
+      .querySelector('.temperature-heading')
+      .classList.remove('temperature-heading-converted');
+    document
+      .querySelector('.convert-button')
+      .classList.remove('convert-button-converted');
+  }, 300);
 }
