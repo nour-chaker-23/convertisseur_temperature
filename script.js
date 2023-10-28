bd86715fa14aebe59a11d1982bd0130331e8534c
nour-chaker-23 <147884647+nour-chaker-23@users.noreply.github.com>
2023-10-28 07:47:26 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,5 @@
+function convertTemperature() {
+  var celsius = parseFloat(document.getElementById("celsius").value);
+  var fahrenheit = (celsius * 9/5) + 32;
+  document.getElementById("fahrenheit").value = fahrenheit;
+}
