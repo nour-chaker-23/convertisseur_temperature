04f3e470208676bb543cfe677abf5313c5ff30e4
Racem triki <trikiracem0@gmail.com>
2023-10-28 11:34:03 +0200

    Sauvegarde des préférences

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function convertTemperature() {
   var fahrenheit = (celsius * 9/5) + 32;
   document.getElementById("fahrenheit").value = fahrenheit;
 }
+
+// sauvegarder les préférences
+function savePreferences() {
+  var selectedUnit = document.getElementById("units").value;
+  localStorage.setItem("defaultUnit", selectedUnit);
+  alert("Préférences sauvegardées avec succès !");
+}   
+// Fonction pour charger les préférences sauvegardées
+window.onload = function() {
+  var defaultUnit = localStorage.getItem("defaultUnit");
+  if (defaultUnit) {
+    document.getElementById("units").value = defaultUnit;
+  }
+}
