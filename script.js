49a6854bd6dbd8f2ca982ec4fe3e56084742a315
Manar khemakhem <manarkhemakhem0@gmail.com>
2023-10-28 10:22:55 +0100

    ajouter la partie de validation des entrées user

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function convertTemperature() {
   var fahrenheit = (celsius * 9/5) + 32;
   document.getElementById("fahrenheit").value = fahrenheit;
 }
+function convertTemperature() {
+  var celsiusInput = document.getElementById("celsius");
+  var celsiusValue = parseFloat(celsiusInput.value);
+
+  // Vérifie si la valeur est un nombre
+  if (isNaN(celsiusValue)) {
+    // Affiche un message d'erreur si ce n'est pas un nombre
+    alert("Veuillez entrer une valeur numérique pour les degrés Celsius.");
+    // Efface la valeur incorrecte
+    celsiusInput.value = "";
+    // Arrête l'exécution de la fonction
+    return;
+  }
+  var fahrenheit = (celsiusValue * 9/5) + 32;
+  document.getElementById("fahrenheit").value = fahrenheit;
+}
