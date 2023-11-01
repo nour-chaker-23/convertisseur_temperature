cddae9b31223ccf87632710bd6d473a81f3285a6
AmalKALLEL3 <amalkallel2001@gmail.com>
2023-11-01 09:05:11 +0100

    [ci-11/feat/amal]:ERREUR

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function convertTemperature() {
-  var celsius = parseFloat(document.getElementById("celsius").value);
-  var fahrenheit = (celsius * 9/5) + 32;
-  document.getElementById("fahrenheit").value = fahrenheit;
+  const celsiusInput = document.getElementById("celsius");
+  const fahrenheitInput = document.getElementById("fahrenheit");
+  const errorMessage = document.getElementById("error-message");
+
+  const celsiusValue = parseFloat(celsiusInput.value);
+
+  if (isNaN(celsiusValue)) {
+    errorMessage.textContent = "Erreur : Veuillez saisir une valeur numerique en Celsius.";
+    fahrenheitInput.value = ""; // Effacer la sortie
+  } else {
+    // Effectuer la conversion de Celsius à Fahrenheit
+    var celsius = parseFloat(document.getElementById("celsius").value);
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit;
+    errorMessage.textContent = ""; // Effacer tout message d'erreur précédent
+  }
 }
